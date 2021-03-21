bf47349d407abe13b0700eaf6259c98ea9c17e4a
amandarwagner <amandarossatto96@gmail.com>
2021-03-21 14:20:57 -0300

    feat(main): create program

--- 00_PiscaLed/PiscaLed/main.asm
+++ 00_PiscaLed/PiscaLed/main.asm
@@ -8,8 +8,22 @@
 
 ; Replace with your application code
 start:
-    inc r16, 0xFF
+    ldi R16, 0xFF
 	out DDRB, R16;
 loop:
 	sbi PORTB, 5
-	rcall 
+	rcall delay_05
+	cbi PORTB, 5
+	rcall delay_05
+	rjmp loop
+delay_05:
+	ldi R16,8
+loop1:
+	ldi R24, low(3037)
+	ldi R25, high(3037)
+delay_loop:
+	adiw R24, 1
+	brne delay_loop
+	dec R16
+	brne loop1
+	ret
