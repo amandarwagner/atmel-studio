baae41188747f711ad8e8696a4230c0ef2a430b4
amandarwagner <amandarossatto96@gmail.com>
2021-03-30 21:33:35 -0300

    refactor(main): delete jumps and add more comments

--- 01_Mult/Mult/main.asm
+++ 01_Mult/Mult/main.asm
@@ -15,21 +15,19 @@ start:
 
 checar_bit_y:
 	LDI r22, 1 ; registrador 0000 0001
-	AND r22, r17
+	AND r22, r17 ; comparar com último bit do r17
 	BREQ shift_z ; se o bit menos significativo do y for 0, só shiftar o Z
-	JMP soma ; se o bit menos significativo do y for 1, somar x no Z e depois shiftar
 
 soma:
-	ADC r19, r16 ; soma o X no Z high >> checar o carry da soma
-	JMP shift_z ; shifta o Z
+	ADC r19, r16 ; soma o X no Z high
 
 shift_z:
 	ROR r19 ; rotar o Z high
-	ROR r18 ; rotar o Z low
-	ROR r17;
+	ROR r18 ; rotar o Z low 
+	ROR r17 ; pegar o último bit do r17
 	SUBI r20,1
 	TST r20
-	BRNE checar_bit_y
+	BRNE checar_bit_y ; se o contador não for 0, olhar o prox bit do y
 
 fim:
 	RJMP fim ; loop quando acabar
